b786d51af823ffcb89c6f0ac292e95cde116ae3c
박상수 <koreatkdtn@naver.com>
2021-02-01 23:48:47 +0900

    junit실습1

---
+++ src/main/java/com/dadu/book/springboot/Application.java
@@ -0,0 +1,11 @@
+package com.dadu.book.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/dadu/book/springboot/web/HelloController.java
@@ -0,0 +1,4 @@
+package com.dadu.book.springboot.web;
+
+public class HelloController {
+}
