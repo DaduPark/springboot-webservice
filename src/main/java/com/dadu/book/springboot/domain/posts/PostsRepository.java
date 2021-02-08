faf0edabd0203bd8183e5c6f67a77ec33a337499
박상수 <koreatkdtn@naver.com>
2021-02-09 00:02:36 +0900

    jpa시작

---
+++ src/main/java/com/dadu/book/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.dadu.book.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository  extends JpaRepository<Posts,Long> {
+}
